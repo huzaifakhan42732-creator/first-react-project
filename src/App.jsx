63b390203762f4efc5099853cc38074292f7ecec
huzaifakhan42732-creator <huzaifakhan42732@gmail.com>
2025-10-07 11:17:25 +0500

    Project
    React first project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from `react`
+import rectlogo from `./assets/react.svg`
+import vitelogo from `/vite.svg`
+import `./App.css`
+
+function App(){
+  const [count, setCount] = useState(0)
+
+  return(
+    <>
+    <h1>My web page</h1>
+
+    </>
+  )
+}
+
+export default App
